a1c544fd3c53bc7ddbe40a9c40e50545faa942ef
Sam Rose <samrose3@gmail.com>
2015-11-28 22:07:02 -0500

    Initial Commit

---
+++ StudyLoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyLoop
+//
+//  Created by Sam Rose on 11/28/15.
+//  Copyright © 2015 StudyLoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
