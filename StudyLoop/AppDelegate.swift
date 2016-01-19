04ac52223c3fb3f5e1bea5692902db4b07bc3a53
Sam Rose <samrose3@gmail.com>
2016-01-19 07:58:50 -0500

    prepared certs and settings for Push Notifications

--- StudyLoop/AppDelegate.swift
+++ StudyLoop/AppDelegate.swift
@@ -1,44 +1,52 @@
  //
-//  AppDelegate.swift
-//  StudyLoop
-//
-//  Created by Sam Rose on 11/28/15.
-//  Copyright © 2015 StudyLoop. All rights reserved.
-//
-
-import UIKit
-import FBSDKCoreKit
-import FBSDKLoginKit
-
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+ //  AppDelegate.swift
+ //  StudyLoop
+ //
+ //  Created by Sam Rose on 11/28/15.
+ //  Copyright © 2015 StudyLoop. All rights reserved.
+ //
+ 
+ import UIKit
+ import FBSDKCoreKit
+ import FBSDKLoginKit
+ 
+ @UIApplicationMain
+ class AppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Set status bar to light theme
         UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
+        // Register for push
+        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+        
+        // return the Facebook SDK delegates
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
-            FBSDKAppEvents.activateApp()
+        FBSDKAppEvents.activateApp()
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
@@ -46,4 +54,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
         return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
-}
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("Got token data! \(deviceToken)! Now send it to the database!")
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("Couldn't register: \(error). :(")
+    }
+ }
